b77ef1e87c43a6c3abec2fb579db30195cf64711
fedlviv <fed.lviv@gmail.com>
2018-03-05 10:38:35 +0200

    Update security (enable global method security)

--- src/main/java/net/omisoft/rest/configuration/security/SecurityConfiguration.java
+++ src/main/java/net/omisoft/rest/configuration/security/SecurityConfiguration.java
@@ -2,22 +2,18 @@ package net.omisoft.rest.configuration.security;
 
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import static net.omisoft.rest.ApplicationConstants.API_V1_BASE_PATH;
-
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled=true, jsr250Enabled=true)
 @AllArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final RestAuthenticationEntryPoint authenticationEntryPoint;
-
-    private static final String[] SWAGGER_PATH = {
-            "/swagger-resources/**",
-            "/swagger-ui.html",
-            "/v2/api-docs"
-    };
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,12 +21,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.headers().frameOptions().disable();
         http
                 .csrf().disable()
-                .authorizeRequests()
-                .antMatchers(SWAGGER_PATH).permitAll()
-                //TODO permission only ADMIN
-                .antMatchers(API_V1_BASE_PATH + "**").hasRole("ADMIN")
-                .anyRequest().permitAll()
-                .and()
                 .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
     }
 
