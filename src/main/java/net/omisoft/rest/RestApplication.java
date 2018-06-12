58e9cf0c440a9b151b6d39fe8cc69e7e7006c047
fedlviv <fed.lviv@gmail.com>
2018-06-12 12:01:02 +0300

    Add Cache, add Encache, update properties (default, test)

--- src/main/java/net/omisoft/rest/RestApplication.java
+++ src/main/java/net/omisoft/rest/RestApplication.java
@@ -3,14 +3,16 @@ package net.omisoft.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication springApplication = new SpringApplication(RestApplication.class);
-		springApplication.addListeners(new ApplicationPidFileWriter());
-		springApplication.run(args);
-	}
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(RestApplication.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
+    }
 
 }
