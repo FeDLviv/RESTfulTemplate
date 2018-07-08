a25a84aa497fde5970fe54c8bb76fb04b58a4108
fedlviv <fed.lviv@gmail.com>
2018-07-08 19:02:22 +0300

    Set default time zone - UTC

--- src/main/java/net/omisoft/rest/RestApplication.java
+++ src/main/java/net/omisoft/rest/RestApplication.java
@@ -5,9 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.cache.annotation.EnableCaching;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableCaching
 public class RestApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(RestApplication.class);
