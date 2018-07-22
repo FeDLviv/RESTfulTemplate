743baddf0bd7044c1a734b5b78da37de0cf534be
fedlviv <fed.lviv@gmail.com>
2018-07-22 16:36:05 +0300

    Update MailService - add operation (sendWithHtmlContent), add thymeleaf

--- src/main/java/net/omisoft/rest/service/mail/MailService.java
+++ src/main/java/net/omisoft/rest/service/mail/MailService.java
@@ -1,9 +1,13 @@
 package net.omisoft.rest.service.mail;
 
+import org.thymeleaf.context.Context;
+
 public interface MailService {
 
     void send(String mailTo, String title, String text);
 
     void sendWithAttachment(String mailTo, String title, String text, String pathToAttachment);
+
+    void sendWithHtmlContent(String mailTo, String title, Context html);
 
 }
--- src/main/java/net/omisoft/rest/service/mail/MailServiceImpl.java
+++ src/main/java/net/omisoft/rest/service/mail/MailServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -19,9 +21,9 @@ import java.io.File;
 public class MailServiceImpl implements MailService {
 
     private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
     private final Environment environment;
     private final MessageSourceConfiguration message;
-
 
     @Override
     public void send(String mailTo, String title, String text) {
@@ -46,6 +48,21 @@ public class MailServiceImpl implements MailService {
                     = new FileSystemResource(new File(pathToAttachment));
             //TODO change file name
             helper.addAttachment("test.png", file);
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            throw new BadRequestException(message.getMessage("exception.email.send"));
+        }
+    }
+
+    @Override
+    public void sendWithHtmlContent(String mailTo, String title, Context html) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(environment.getProperty("spring.mail.username"));
+            helper.setTo(mailTo);
+            helper.setSubject(title);
+            helper.setText(templateEngine.process("mailTemplate", html), true);
             mailSender.send(message);
         } catch (MessagingException e) {
             throw new BadRequestException(message.getMessage("exception.email.send"));
