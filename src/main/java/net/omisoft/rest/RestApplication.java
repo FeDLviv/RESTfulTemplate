61676dacfb0b23c585d8acf29cc04e4815395df3
fedlviv <fed.lviv@gmail.com>
2018-07-28 19:04:01 +0300

    Update gradle - add library (aop, retry), update version (swagger, jjwt, aws)

--- src/main/java/net/omisoft/rest/RestApplication.java
+++ src/main/java/net/omisoft/rest/RestApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.retry.annotation.EnableRetry;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableCaching
+@EnableRetry
 public class RestApplication {
 
     @PostConstruct
