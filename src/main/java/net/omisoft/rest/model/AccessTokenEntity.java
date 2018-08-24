64a4d1f7b3113dd10b69d3f9ca4b33b60b700576
fedlviv <fed.lviv@gmail.com>
2018-08-24 12:01:46 +0300

    Update DB - change table (access_tokens - add column), update cache - change (ehcahe.xml, application.properties), add entity - AccessToken

---
+++ src/main/java/net/omisoft/rest/model/AccessTokenEntity.java
@@ -0,0 +1,34 @@
+package net.omisoft.rest.model;
+
+import lombok.*;
+import net.omisoft.rest.model.base.BaseEntity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "access_tokens")
+@AttributeOverrides({
+        @AttributeOverride(name = "id", column = @Column(name = "id_token"))
+})
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class AccessTokenEntity extends BaseEntity {
+
+    @NonNull
+    @Column(name = "token", nullable = false, unique = true)
+    private String token;
+
+    @NonNull
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_user", nullable = false)
+    private UserEntity user;
+
+    @NonNull
+    @Column(name = "expired_date", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date expired;
+
+}
