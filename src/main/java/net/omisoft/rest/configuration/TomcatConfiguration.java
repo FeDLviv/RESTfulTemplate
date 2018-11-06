75f703f222873b49805a461cbf8e4c0c65a83795
fedlviv <fed.lviv@gmail.com>
2018-11-06 18:25:07 +0200

    Update configuration - Tomcat (set max swallow size)

--- src/main/java/net/omisoft/rest/configuration/TomcatConfiguration.java
+++ src/main/java/net/omisoft/rest/configuration/TomcatConfiguration.java
@@ -2,8 +2,11 @@ package net.omisoft.rest.configuration;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -11,30 +14,45 @@ import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Conditional(HTTPSConditional.class)
 public class TomcatConfiguration {
 
     @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+    public ServletWebServerFactory servletContainer(@Autowired(required = false) Connector redirectConnector) {
+        TomcatServletWebServerFactory factory;
+        if (redirectConnector != null) {
+            factory = new TomcatServletWebServerFactory() {
 
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection securityCollection = new SecurityCollection();
-                securityCollection.addPattern("/*");
-                securityConstraint.addCollection(securityCollection);
-                context.addConstraint(securityConstraint);
-            }
+                @Override
+                protected void postProcessContext(Context context) {
+                    SecurityConstraint securityConstraint = new SecurityConstraint();
+                    securityConstraint.setUserConstraint("CONFIDENTIAL");
+                    SecurityCollection securityCollection = new SecurityCollection();
+                    securityCollection.addPattern("/*");
+                    securityConstraint.addCollection(securityCollection);
+                    context.addConstraint(securityConstraint);
+                }
 
-        };
-
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
-        return tomcat;
+            };
+            factory.addAdditionalTomcatConnectors(redirectConnector());
+        } else {
+            factory = new TomcatServletWebServerFactory();
+        }
+        factory.addConnectorCustomizers(customizer());
+        return factory;
     }
 
-    private Connector redirectConnector() {
+    public TomcatConnectorCustomizer customizer() {
+        return new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+            }
+        };
+    }
+
+    @Bean
+    @Conditional(HTTPSConditional.class)
+    public Connector redirectConnector() {
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
         connector.setPort(80);
@@ -44,4 +62,3 @@ public class TomcatConfiguration {
     }
 
 }
-
