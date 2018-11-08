09067681920d0d7b59f43994c3c98a00c382b0d2
fedlviv <fed.lviv@gmail.com>
2018-11-08 09:28:13 +0200

    Add annotation - RemoteIp

---
+++ src/main/java/net/omisoft/rest/configuration/annotation/RemoteIp.java
@@ -0,0 +1,12 @@
+package net.omisoft.rest.configuration.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface RemoteIp {
+
+}
