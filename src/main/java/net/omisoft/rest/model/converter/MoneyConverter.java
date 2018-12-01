73454befbe636888a399c6977b5c8fb1dc188ff3
fedlviv <fed.lviv@gmail.com>
2018-12-01 21:31:12 +0200

    Update converter - Money (add RoundingMode -> HALF_EVEN)

--- src/main/java/net/omisoft/rest/model/converter/MoneyConverter.java
+++ src/main/java/net/omisoft/rest/model/converter/MoneyConverter.java
@@ -3,6 +3,7 @@ package net.omisoft.rest.model.converter;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Converter
 public class MoneyConverter implements AttributeConverter<BigDecimal, BigDecimal> {
@@ -14,7 +15,7 @@ public class MoneyConverter implements AttributeConverter<BigDecimal, BigDecimal
 
     @Override
     public BigDecimal convertToEntityAttribute(BigDecimal dbData) {
-        return dbData.setScale(2);
+        return dbData.setScale(2, RoundingMode.HALF_EVEN);
     }
 
 }
