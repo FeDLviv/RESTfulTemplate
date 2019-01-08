6b74aa5cc26ad32c37b51d894c17262b6d5f3f64
FeDLviv <fed.lviv@gmail.com>
2019-01-08 23:52:18 +0200

    Update DTO - UserCreate (add validation -> password not null)

--- src/main/java/net/omisoft/rest/dto/user/UserCreateDto.java
+++ src/main/java/net/omisoft/rest/dto/user/UserCreateDto.java
@@ -9,6 +9,7 @@ import net.omisoft.rest.dto.validator.annotation.ValidateNotCyrillic;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import static net.omisoft.rest.ApplicationConstants.PASSWORD_MAX_LENGTH;
@@ -27,6 +28,7 @@ public class UserCreateDto {
     @ApiModelProperty(notes = "Email", value = "fed.lviv@gmail.com", example = "fed.lviv@gmail.com", required = true, position = 0)
     private String email;
 
+    @NotNull
     @ValidateNotCyrillic
     @Size(min = PASSWORD_MIN_LENGTH, max = PASSWORD_MAX_LENGTH)
     @ApiModelProperty(notes = "Password", value = "1234", example = "1234", required = true, position = 1)
