55a5108f6b938cbb83bcf0149fbdf5b23b789964
FeDLviv <fed.lviv@gmail.com>
2019-01-08 16:50:21 +0200

    Add DTO - UserCreate

---
+++ src/main/java/net/omisoft/rest/dto/user/UserCreateDto.java
@@ -0,0 +1,35 @@
+package net.omisoft.rest.dto.user;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.omisoft.rest.dto.validator.annotation.ValidateNotCyrillic;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import static net.omisoft.rest.ApplicationConstants.PASSWORD_MAX_LENGTH;
+import static net.omisoft.rest.ApplicationConstants.PASSWORD_MIN_LENGTH;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserCreateDto {
+
+    @NotBlank
+    @Size(max = 100)
+    @Email
+    @ValidateNotCyrillic
+    @ApiModelProperty(notes = "Email", value = "fed.lviv@gmail.com", example = "fed.lviv@gmail.com", required = true, position = 0)
+    private String email;
+
+    @ValidateNotCyrillic
+    @Size(min = PASSWORD_MIN_LENGTH, max = PASSWORD_MAX_LENGTH)
+    @ApiModelProperty(notes = "Password", value = "1234", example = "1234", required = true, position = 1)
+    private String password;
+
+}
