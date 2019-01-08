7f339c2642000159d9c1668973b53625cd29ae7e
FeDLviv <fed.lviv@gmail.com>
2019-01-08 16:00:02 +0200

    Update DTO - CustomId (HATEOAS) + lombok.config

--- src/main/java/net/omisoft/rest/pojo/CustomId.java
+++ src/main/java/net/omisoft/rest/pojo/CustomId.java
@@ -1,16 +1,19 @@
 package net.omisoft.rest.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.hateoas.ResourceSupport;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CustomId  {
+public class CustomId extends ResourceSupport {
 
+    @JsonProperty(value = "id")
     @ApiModelProperty(notes = "Id", required = true, position = 0)
-    private long id;
+    private long identifier;
 
 }
