994d4541ae047a030b1163a67f698b3bc4d23251
FeDLviv <fed.lviv@gmail.com>
2019-01-12 22:51:47 +0200

    Update entity - User (add payments)

--- src/main/java/net/omisoft/rest/model/UserEntity.java
+++ src/main/java/net/omisoft/rest/model/UserEntity.java
@@ -2,19 +2,20 @@ package net.omisoft.rest.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import net.omisoft.rest.model.base.BaseEntity;
 import net.omisoft.rest.model.base.UserRole;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
 @AttributeOverrides({
         @AttributeOverride(name = "id", column = @Column(name = "id_user"))
 })
-//TODO @EqualsAndHashCode(exclude = {"XXX"})
-@EqualsAndHashCode(callSuper = true)
-//TODO @ToString(exclude = {"XXX"})
+@EqualsAndHashCode(callSuper = true, exclude = {"payments"})
+@ToString(exclude = {"payments"})
 @Data
 public class UserEntity extends BaseEntity {
 
@@ -27,5 +28,8 @@ public class UserEntity extends BaseEntity {
     @Enumerated(value = EnumType.STRING)
     @Column(name = "role", nullable = false)
     private UserRole role;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    public Set<PaymentEntity> payments;
 
 }
