5e411b16a6fa0f6e75da44a11dff91e55b0435d8
FeDLviv <fed.lviv@gmail.com>
2019-06-11 18:05:55 +0300

    Update annotation - Validate enum (repeatable -> List)

--- src/main/java/net/omisoft/rest/dto/validator/annotation/ValidateEnum.java
+++ src/main/java/net/omisoft/rest/dto/validator/annotation/ValidateEnum.java
@@ -6,10 +6,13 @@ import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.*;
 
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE_USE})
 @Documented
 @Constraint(validatedBy = EnumValidator.class)
+@Repeatable(ValidateEnum.List.class)
 public @interface ValidateEnum {
 
     String message() default "exception.enum.wrong";
@@ -25,5 +28,12 @@ public @interface ValidateEnum {
     String[] include() default {};
 
     boolean ignoreCase() default false;
+
+    @Retention(RUNTIME)
+    @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE_USE})
+    @Documented
+    @interface List {
+        ValidateEnum[] value();
+    }
 
 }
