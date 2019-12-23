46777e4b95d0b71099f554f057e7acdc9e10f979
FeDLviv <fed.lviv@gmail.com>
2019-12-23 18:23:44 +0200

    Add Spring Boot Admin (client)

--- src/main/java/net/omisoft/rest/configuration/security/SecurityConfiguration.java
+++ src/main/java/net/omisoft/rest/configuration/security/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package net.omisoft.rest.configuration.security;
 
-import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -10,6 +9,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.Objects;
 import java.util.stream.Stream;
 
@@ -18,13 +21,35 @@ import static net.omisoft.rest.ApplicationConstants.PROFILE_PROD;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
-@AllArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final JWTAuthenticationFilter jwtFilter;
     private final CustomAuthenticationEntryPoint authenticationEntryPoint;
     private final CustomAccessDeniedHandler accessDeniedHandler;
     private final Environment environment;
+    private final String accessExpresion;
+
+    public SecurityConfiguration(JWTAuthenticationFilter jwtFilter,
+                                 CustomAuthenticationEntryPoint authenticationEntryPoint,
+                                 CustomAccessDeniedHandler accessDeniedHandler,
+                                 Environment environment) throws MalformedURLException, UnknownHostException {
+        this.jwtFilter = jwtFilter;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.accessDeniedHandler = accessDeniedHandler;
+        this.environment = environment;
+        this.accessExpresion = generateAccessExpression(environment.getProperty("spring.boot.admin.client.url"));
+    }
+
+    private String generateAccessExpression(String url) throws MalformedURLException, UnknownHostException {
+        //TODO change role
+        if (url != null) {
+            InetAddress address = InetAddress.getByName(new URL(url).getHost());
+            String ip = address.getHostAddress();
+            return "hasRole('ADMIN') or hasIpAddress('" + ip + "')";
+        } else {
+            return "hasRole('ADMIN')";
+        }
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -36,11 +61,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
         http.exceptionHandling().accessDeniedHandler(accessDeniedHandler);
-        //TODO change role
         if (Stream.of(environment.getActiveProfiles()).anyMatch(profile -> Objects.equals(profile, PROFILE_PROD))) {
             http.authorizeRequests()
                     .antMatchers(environment.getProperty("management.endpoints.web.base-path") + "/**")
-                    .hasAnyRole("ADMIN");
+                    .access(accessExpresion);
         }
     }
 
